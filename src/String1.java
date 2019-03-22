e68f1845a76d28394992dcbc6a6c6ec91a55f19f
喜 杨 <yangxirangwo@outlook.com>
2019-03-23 01:20:44 +0800

    字符串类型使用

---
+++ src/String1.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @author yangxixi
+ *
+ */
+public class String1 {
+	public static void main( String[] args) {
+		String Hello = "ÄãºÃ£¬ÊÀ½ç£¡£¡£¡";
+		System.out.println(Hello + "\n" + "\t" + Hello);
+	}
+
+}
